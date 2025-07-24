cbca8e43f4cd5bf8da6ad67fc3b9fb71565b5c8d
IFloresV <iker.fv@gmail.com>
2025-07-23 21:10:18 -0600

    Feat - Creacion Daos

---
+++ dao/PedidoDAO.js
@@ -0,0 +1,20 @@
+// dao/PedidoDAO.js
+import { query } from "../db/index.js";
+
+export const crearPedido = async (pedido) => {
+   const { nombre, apellidos, correo, telefono, codigopostal, producto, cantidad, precio_unitario, total, pais } =
+      pedido;
+
+   const result = await query(
+      ` INSERT INTO pedidos (nombre, apellidos, correo, telefono,codigopostal, producto, cantidad,precio_unitario, total, pais) 
+        VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)
+        RETURNING *`,
+      [nombre, apellidos, correo, telefono, codigopostal, producto, cantidad, precio_unitario, total, pais],
+   );
+
+   return result.rows[0];
+};
+
+export const actualizarEstadoPedido = async (pedidoId, nuevoEstado) => {
+   await query(`UPDATE pedidos SET estatus_pago = $1 WHERE id = $2`, [nuevoEstado, pedidoId]);
+};
